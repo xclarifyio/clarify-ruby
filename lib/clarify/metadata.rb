2f828c6c2c170d93b9ba462a4734f99bebbabf43
rubygeek <nola@rubygeek.com>
2014-09-06 22:52:22 -0500

    added metadata endpoints

---
+++ lib/clarify/metadata.rb
@@ -0,0 +1,30 @@
+require_relative 'request'
+
+module Clarify
+  class Metadata < Request
+    
+    def initialize(version=1)
+      super(version)
+    end
+
+    def find(bundle_id=nil)
+      raise ArgumentError, "Missing bundle id" if bundle_id.nil?
+      response = self.class.get("/#{version}/bundles/#{bundle_id}/metadata", headers: headers)
+      build_response(response)
+    end
+
+    def delete(bundle_id=nil)
+      raise ArgumentError, "Missing bundle id" if bundle_id.nil?
+      response = self.class.delete("/#{version}/bundles/#{bundle_id}/metadata", headers: headers)
+      build_response(response)
+    end
+
+    def update(bundle_id=nil, data="")
+      raise ArgumentError, "Missing bundle id" if bundle_id.nil?      
+      response = self.class.put("/#{version}/bundles/#{bundle_id}/metadata",
+                                body: {data: data} , headers: headers)
+      build_response(response)
+    end
+
+  end
+end
---
+++ spec/lib/clarify/metadata_spec.rb
@@ -0,0 +1,40 @@
+module Clarify
+
+  describe Metadata do
+    before do
+      bundle = Bundle.new.create
+      @bundle_id = bundle.data[:id]
+    end
+
+    after do
+      Bundle.new.delete(@bundle_id)
+    end
+    
+    it "should retrieve the metadata for bundle" do
+      metadata = Metadata.new.find(@bundle_id)
+      expect(metadata.status).to eql 200
+    end
+
+    it "should update a bundle" do
+      metadata = Metadata.new.update(@bundle_id, '{"meta": 1}')
+      expect(metadata.status).to eql 202
+      expect(metadata.data.keys).to include(:_class, :_links)
+    end
+  end
+
+  context "deleting metadata" do
+    it "should delete the metadata" do
+      bundle = Bundle.new.create
+      bundle_id = bundle.data[:id]
+      
+      Metadata.new.update(bundle_id, '{"meta": 1}')
+      metadata_before  = Metadata.new.find(bundle_id)         
+      expect(metadata_before.data[:data].keys).to include :meta
+
+      metadata_after = Metadata.new.delete(bundle_id)
+      expect(metadata_after.status).to eq 204
+      expect(metadata_after.data).to be_empty
+    end
+  end
+
+end
