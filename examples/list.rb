d546c8cb30a2d7ec8f983fffd2d280fd5f9f26df
Keith Casey <keith@caseysoftware.com>
2014-12-02 12:11:36 -0600

    wired a simple list method

---
+++ examples/list.rb
@@ -0,0 +1,16 @@
+
+require 'rubygems'
+require 'clarify'
+
+# This assumes that you set up your key in the environment using: export CLARIFY_API_KEY=abcde12345
+Clarify.configure do |config|
+  # config.api_key = 'CLARIFY_API_KEY'
+  # if key in ENV
+  config.api_key = ENV['CLARIFY_API_KEY']
+  config.version = 1  #default is latest api version
+end
+
+@client = Clarify::Bundle.new
+
+bundles = @client.find_all.data[:_links][:items]
+bundles.each { |bundle| print bundle[:href]  }
